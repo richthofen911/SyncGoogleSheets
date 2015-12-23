33a18231e4c2fc84db98f04b3f83b26ec2f714b5
Yichao Li <yichaoli.richthofen@gmail.com>
2015-12-23 11:45:37 -0500

    pull data from mysql to the active sheet

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 
-
-var sheet = SpreadsheetApp.getActiveSheet();
-
 var address = '72.143.118.130';
 var rootPwd = 'qwer1234';
 var user = 'root';
@@ -10,9 +7,47 @@ var db = 'test_gs';
 
 var root = 'root';
 var instanceUrl = 'jdbc:mysql://' + address;
-var dbUrl = instanceUrl + '\\' + db;
+var dbUrl = instanceUrl + '/' + db;
 
-function createDataBase(){
-  var conn = Jdbc.getConnection(instanceUrl, root, rootPwd);
-  conn.createStatement().execute('CREATE DATABASE ' + db);
+
+var SQLstatement;
+var conn;
+var queryResult;
+
+
+function getDatabaseConn(){
+  conn = Jdbc.getConnection(dbUrl, root, rootPwd);
+  return conn;
+} 
+  
+
+function pullDataFromDatabase(dbConn, tableName){
+  SQLstatement = dbConn.createStatement();
+  queryResult = SQLstatement.executeQuery("SELECT * FROM " + tableName);
+  return queryResult;
+}
+
+function adaptDataIntoSheet(data, fieldsPerRecord){
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  // choose a range in sheet, usually starts from A2, because A1 and the row are used to show titles(field names)
+  var cell = ss.getRange('A2');
+  
+  var row = 0;
+  while(data.next()) {
+    for(var i = 0; i < fieldsPerRecord; i++) {    
+       cell.offset(row, i).setValue(data.getString(i + 1));
+    }
+    row++;
+  }
+}
+
+function getDataFromMyDB(){
+  adaptDataIntoSheet(pullDataFromDatabase(getDatabaseConn(), "user"), 2);
+  closeConn();
+}
+
+function closeConn(){
+   queryResult.close();
+   SQLstatement.close();
+   conn.close();
 }
